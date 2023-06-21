56ef31a1891cbac05960b5974de61cbb2b294a54
bruno-PC\bruno <bruno.doue@laposte.net>
2023-06-21 21:50:22 +0200

    date resolved

--- partie 2/exo11.php
+++ partie 2/exo11.php
@@ -6,6 +6,29 @@ formaterDateFr("2018-02-23");</p>
 
 
 <?php
-$date = new DateTime('2000-01-01');
-echo $date->format('Y-m-d H:i:s');
+
+$dateSample="2018-02-23";
+
+function displayDateEng(string $date){
+    $newdate = new DateTime($date);
+    echo"Affichage en anglais : " ;
+    echo $newdate->format('l d F Y');
+}
+
+function formaterDateFr(string $date){
+
+    $semaine = array(" Dimanche "," Lundi "," Mardi "," Mercredi "," Jeudi ",
+                " Vendredi "," Samedi ");
+    $mois =array(1=>" janvier "," février "," mars "," avril "," mai "," juin ",
+                " juillet "," août "," septembre "," octobre "," novembre "," décembre ");
+
+    $time = strtotime($date); //convertit la date en numérique (int timestamp)
+    echo "Affichage en français : " ; 
+    echo $semaine[date('w',$time)] ," ",date('j',$time),"", $mois[date('n',$time)], date('Y',$time)," ";
+}
+
+ displayDateEng($dateSample);
+ echo "<br>-------------------------------------------------------<br>";
+ formaterDateFr($dateSample);
+
 ?>
